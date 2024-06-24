501cbb57437fa45a7759adb73cc7040a31ea044a
keko24 <kotevski_andrej@hotmail.com>
2024-06-24 02:45:57 +0200

    Added found to iziis.

--- src/researcher_network_mk/scraper/ukim_scrapers/iziis_inst_name_scraper.py
+++ src/researcher_network_mk/scraper/ukim_scrapers/iziis_inst_name_scraper.py
@@ -2,14 +2,18 @@ import os
 
 import pandas as pd
 
-from researcher_network_mk.utils import get_project_root
 from researcher_network_mk.transliteration import transliterate_cyrillic_to_latin
+from researcher_network_mk.utils import get_project_root
+
 
 def translate_names_to_latin(names_mk):
     return [transliterate_cyrillic_to_latin(name) for name in names_mk]
 
+
 def main():
-    results_path = os.path.join(get_project_root(), "data", "researchers", "ukim", "seizmoloski_institut")
+    results_path = os.path.join(
+        get_project_root(), "data", "researchers", "ukim", "seizmoloski_institut"
+    )
     data_mk = [
         "Марта Стојмановска",
         "Горан Чапрагоски",
@@ -366,17 +370,17 @@ def main():
         "Попоска Марина",
         "Ристовска Христина",
         "Таравари Леарт",
-        "Костиќ Ивана"
+        "Костиќ Ивана",
     ]
 
-# Transliterate the names
+    # Transliterate the names
     data = data_mk
 
     os.makedirs(results_path, exist_ok=True)
     df = pd.DataFrame(data, columns=["name"])
     df["processed"] = False
+    df["found"] = False
     df.to_csv(os.path.join(results_path, "researchers.csv"))
-
 
 
 if __name__ == "__main__":
